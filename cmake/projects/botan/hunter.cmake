a3bcd479f371dece01ad1bca1ccb82e4dd42d375
Joerg-Christian Boehme <joerg@chaosdorf.de>
2018-10-04 17:19:12 +0200

    Add botan build

---
+++ cmake/projects/botan/hunter.cmake
@@ -0,0 +1,32 @@
+# !!! DO NOT PLACE HEADER GUARDS HERE !!!
+
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_download)
+include(hunter_pick_scheme)
+include(hunter_cmake_args)
+
+hunter_add_version(
+    PACKAGE_NAME
+    botan
+    VERSION
+    2.8.0
+    URL
+    "https://github.com/randombit/botan/archive/2.8.0.zip"
+    SHA1
+    d2b828a36dad40b4bf4ccbef594067ca71bfe315
+)
+
+hunter_cmake_args(
+    botan
+    CMAKE_ARGS
+      PKGCONFIG_EXPORT_TARGETS=botan-2
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_botan)
+hunter_cacheable(botan)
+hunter_download(PACKAGE_NAME botan
+  PACKAGE_INTERNAL_DEPS_ID "1"  # Increment for each new pull request
+  PACKAGE_UNRELOCATABLE_TEXT_FILES
+  lib/pkgconfig/botan-2.pc
+)
