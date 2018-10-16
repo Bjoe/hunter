44169c33ec4dd61274abc6fced964aff726fe09a
Joerg-Christian Boehme <joerg@chaosdorf.de>
2018-10-16 17:25:42 +0200

    Cleanup gmp

--- cmake/projects/gmp/hunter.cmake
+++ cmake/projects/gmp/hunter.cmake
@@ -2,8 +2,7 @@
 
 include(hunter_add_version)
 include(hunter_cacheable)
-#include(hunter_cmake_args)
-#include(hunter_configuration_types)
+include(hunter_configuration_types)
 include(hunter_download)
 include(hunter_pick_scheme)
 
@@ -18,25 +17,10 @@ hunter_add_version(
     366ded6a44cd108ba6b3f5b9a252eab3f3a95cdf
 )
 
-#if (ANDROID OR IOS)
-# hunter_cmake_args(
-#    gmp
-#    CMAKE_ARGS
-#    PKGCONFIG_EXPORT_TARGETS=libfoo;libbar
-#    DEPENDS_ON_PACKAGES=foo;bar
-#    EXTRA_FLAGS=--enable-bots
-# )
-#endif()
-
-#hunter_configuration_types(gmp CONFIGURATION_TYPES Release)
+hunter_configuration_types(gmp CONFIGURATION_TYPES Release)
 hunter_pick_scheme(DEFAULT url_sha1_autotools)
 hunter_cacheable(gmp)
 hunter_download(
     PACKAGE_NAME gmp
     PACKAGE_INTERNAL_DEPS_ID "1"
-#     PACKAGE_UNRELOCATABLE_TEXT_FILES
-#     "lib/libfoo.la"
-#     "lib/libpbar.la"
-#     "lib/pkgconfig/libfoo.pc"
-#     "lib/pkgconfig/libpbar.pc"
 )
